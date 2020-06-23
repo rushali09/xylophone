485b307791813e788f5a9f40aecbfb6aa09899e0
rushali09 <rushalisreedhar37@gmail.com>
2020-06-23 16:57:15 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,9 @@ class XylophoneApp extends StatelessWidget {
   void playSound(int soundNumber){ //()==> specifying the number of the track
       final player = AudioCache();
       player.play('note$soundNumber.wav');
-      }
-  Expanded buildKey({Color color, int soundNumber}){
-        return Expanded(
-          child: FlatButton(
-            onPressed: (){
-              playSound(soundNumber);
-            }, 
-            child: Text('Click ME'),
-            ) ,
-        );
-  }
-
+      
   
-
-
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,19 +21,80 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                buildKey(color:Colors.red, soundNumber:1),
-                buildKey(color:Colors.yellow, soundNumber:2),
-                buildKey(color:Colors.blue, soundNumber:3),
-                buildKey(color:Colors.pink, soundNumber:4),
-                buildKey(color:Colors.green, soundNumber:5),
-                buildKey(color:Colors.purple, soundNumber:6),
-                buildKey(color:Colors.teal, soundNumber:7),
-                              ],
-                            ),
-                        ),
-                      ),
-                    );
-                  }
+                  Expanded(
+                      child: FlatButton(
+                       color: Colors.red,
+                        onPressed: (){
+                              playSound(1);
+                        }, 
+                         child: Text(''),
+                      ) ,
+                ),
+           
+
+                  Expanded(
+                      child: FlatButton(
+                       color: Colors.yellow,
+                        onPressed: (){
+                              playSound(2);
+                        }, 
+                         child: Text(''),
+                      ) ,
+                ),
+
+
+                    Expanded(
+                     child: FlatButton(
+                       color: Colors.pink[100],
+                        onPressed: (){
+                              playSound(3);
+                        }, 
+                         child: Text(''),
+                      ) ,
+                ),
+                      
+                    Expanded(
+                     child: FlatButton(
+                       color: Colors.blue,
+                        onPressed: (){
+                              playSound(4);
+                        }, 
+                         child: Text(''),
+                      ) ,
+                ),
+                    Expanded(
+                     child: FlatButton(
+                       color: Colors.orange,
+                        onPressed: (){
+                              playSound(5);
+                        }, 
+                         child: Text(''),
+                      ) ,
+                ),
+                    Expanded(
+                     child: FlatButton(
+                       color: Colors.purple,
+                        onPressed: (){
+                              playSound(6);
+                        }, 
+                         child: Text(''),
+                      ) ,
+                ),
+                      Expanded(
+                     child: FlatButton(
+                       color: Colors.green,
+                        onPressed: (){
+                              playSound(7);
+                        }, 
+                         child: Text(''),
+                      ) ,
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
                 
                  
 }
